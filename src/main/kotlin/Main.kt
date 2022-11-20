6c132a6dd3c140a2b0525f5ef08229c30d33ecc6
Ilya Brant <detroitib31@gmail.com>
2022-11-21 04:04:49 +0600

    Написал вычисления

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,6 +3,17 @@ import java.util.Scanner
 fun main(args: Array<String>) {
 
     val reader = Scanner(System.`in`)
+
     print("Введите цисло: ")
     val n = reader.nextInt()
+    var f = 0
+
+    if (n > 0) {
+        f = 1
+    }
+    for (i in (2..n)) {
+        f *= i
+    }
+    print("Факториал числа: $n = $f")
+
 }
